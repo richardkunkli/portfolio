0bfd6db17f89587a43b9a1d747f8a5a37aeb6c6b
KRicsii <kunklirichard@gmail.com>
2022-08-25 17:39:22 +0200

    fix logo name

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
       <header className="w-full flex flex-col gap-2 md:bg-neutral-900 md:px-10 pb-4">
         <div className="flex flex-row justify-between items-start">
           <div className="h-12 w-12 relative mt-4 mb-2 md:mt-10">
-            <Image alt="Logo" src="/logoMedium.svg" height={48} width={48} />
+            <Image alt="Logo" src="/LogoMedium.svg" height={48} width={48} />
           </div>
           <div className={`h-14 w-20 md:h-20 ${styles.dotGrid}`}></div>
         </div>
