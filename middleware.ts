df30c5eda3d896b5cfe2ac5e0e1046478082b296
KRicsii <kunklirichard@gmail.com>
2022-09-28 18:17:23 +0200

    beta site and auth

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ['/', '/index'],
-}
+  matcher: ["/beta"],
+};
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
 
   if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
-      return NextResponse.next()
+    if (user === "4dmin" && pwd === "testpwd123") {
+      return NextResponse.next();
     }
   }
-  url.pathname = '/beta'
+  url.pathname = "/api/auth";
 
-  return NextResponse.rewrite(url)
+  return NextResponse.rewrite(url);
 }
